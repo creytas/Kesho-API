53387c4be11f1dccc176aa998f88160027409b60
CR3Y745 <creytas@gmail.com>
2022-02-15 11:54:06 +0100

    refactor: add user id

--- middleware/user/user.validator.status.middleware.js
+++ middleware/user/user.validator.status.middleware.js
@@ -1,29 +1,28 @@
 const express = require("express");
-const { body, query,validationResult } = require("express-validator");
+const { body, query, validationResult } = require("express-validator");
 
 const userValidatorStatusMiddleware = express();
 
 userValidatorStatusMiddleware.use(
   [
-    query("id_user")
+    query("id")
       .notEmpty()
       .withMessage("Cannot be empty")
       .matches(/\d/)
       .withMessage("must contain a number"),
     body("statut")
-    .notEmpty()
-    .withMessage("Cannot be empty")
-    .matches(/\w/)
-    .withMessage("must contain a number"),
+      .notEmpty()
+      .withMessage("Cannot be empty")
+      .matches(/\w/)
+      .withMessage("must contain a number"),
   ],
   (req, res, next) => {
-
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.id_user = req.query.id_user;
+    res.id = req.query.id;
     res.statut = req.body.statut;
     next();
   }
