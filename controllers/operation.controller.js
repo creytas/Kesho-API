ceec59d20216c2b80fb21229c83e676af859046a
CR3Y745 <creytas@gmail.com>
2022-06-17 02:10:15 +0100

    feat:create stock current state

--- controllers/operation.controller.js
+++ controllers/operation.controller.js
@@ -122,6 +122,173 @@ const getOperationByDate = async (req, res, next) => {
       res.status(500).send({ message: error.message });
     });
 };
+const getMaterialState = async (req, res) => {
+  const cornEntrance = await sequelize.query(queries.corn_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const cornExit = await sequelize.query(queries.corn_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const sorghumEntrance = await sequelize.query(queries.sorghum_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const sorghumExit = await sequelize.query(queries.sorghum_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const soyEntrance = await sequelize.query(queries.soy_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const soyExit = await sequelize.query(queries.soy_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const sugarEntrance = await sequelize.query(queries.sugar_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const sugarExit = await sequelize.query(queries.sugar_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const soapEntrance = await sequelize.query(queries.soap_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const soapExit = await sequelize.query(queries.soap_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const oilEntrance = await sequelize.query(queries.oil_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const oilExit = await sequelize.query(queries.oil_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const breadEntrance = await sequelize.query(queries.bread_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const breadExit = await sequelize.query(queries.bread_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const clothesEntrance = await sequelize.query(queries.clothes_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const clothesExit = await sequelize.query(queries.clothes_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const foodEntrance = await sequelize.query(queries.food_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const foodExit = await sequelize.query(queries.food_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const toyEntrance = await sequelize.query(queries.toy_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const toyExit = await sequelize.query(queries.toy_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const shoesEntrance = await sequelize.query(queries.shoes_entrance, {
+    type: QueryTypes.SELECT,
+  });
+  const shoesExit = await sequelize.query(queries.shoes_exit, {
+    type: QueryTypes.SELECT,
+  });
+  const energyBriquetteEntrance = await sequelize.query(
+    queries.energy_briquette_entrance,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+  const energyBriquetteExit = await sequelize.query(
+    queries.energy_briquette_exit,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+  const leafExtractEntrance = await sequelize.query(
+    queries.leaf_extract_entrance,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+  const leafExtractExit = await sequelize.query(queries.leaf_extract_exit, {
+    type: QueryTypes.SELECT,
+  });
+
+  const data = [
+    {
+      designation: cornEntrance ? cornEntrance.libelle_matiere : "maïs",
+      entrance: cornEntrance ? cornEntrance.total : 0,
+      exit: cornExit ? cornExit.total : 0,
+    },
+    {
+      designation: sorghumEntrance ? sorghumEntrance.libelle_matiere : "sorgho",
+      entrance: sorghumEntrance ? sorghumEntrance.total : 0,
+      exit: sorghumExit ? sorghumExit.total : 0,
+    },
+    {
+      designation: soyEntrance ? soyEntrance.libelle_matiere : "soja",
+      entrance: soyEntrance ? soyEntrance.total : 0,
+      exit: soyExit ? soyExit.total : 0,
+    },
+    {
+      designation: sugarEntrance ? sugarEntrance.libelle_matiere : "sucre",
+      entrance: sugarEntrance ? sugarEntrance.total : 0,
+      exit: sugarExit ? sugarExit.total : 0,
+    },
+    {
+      designation: soapEntrance ? soapEntrance.libelle_matiere : "savon Omo",
+      entrance: soapEntrance ? soapEntrance.total : 0,
+      exit: soapExit ? soapExit.total : 0,
+    },
+    {
+      designation: oilEntrance ? oilEntrance.libelle_matiere : "huile végétale",
+      entrance: oilEntrance ? oilEntrance.total : 0,
+      exit: oilExit ? oilExit.total : 0,
+    },
+    {
+      designation: breadEntrance
+        ? breadEntrance.libelle_matiere
+        : "pains/biscuits",
+      entrance: breadEntrance ? breadEntrance.total : 0,
+      exit: breadExit ? breadExit.total : 0,
+    },
+    {
+      designation: foodEntrance ? foodEntrance.libelle_matiere : "nourriture",
+      entrance: foodEntrance ? foodEntrance.total : 0,
+      exit: foodExit ? foodExit.total : 0,
+    },
+    {
+      designation: clothesEntrance
+        ? clothesEntrance.libelle_matiere
+        : "vêtements",
+      entrance: clothesEntrance ? clothesEntrance.total : 0,
+      exit: clothesExit ? clothesExit.total : 0,
+    },
+    {
+      designation: toyEntrance ? toyEntrance.libelle_matiere : "jouets",
+      entrance: toyEntrance ? toyEntrance.total : 0,
+      exit: toyExit ? toyExit.total : 0,
+    },
+    {
+      designation: shoesEntrance ? shoesEntrance.libelle_matiere : "chaussures",
+      entrance: shoesEntrance ? shoesEntrance.total : 0,
+      exit: shoesExit ? shoesExit.total : 0,
+    },
+    {
+      designation: energyBriquetteEntrance
+        ? energyBriquetteEntrance.libelle_matiere
+        : "briq. énerg",
+      entrance: energyBriquetteEntrance ? energyBriquetteEntrance.total : 0,
+      exit: energyBriquetteExit ? energyBriquetteExit.total : 0,
+    },
+    {
+      designation: leafExtractEntrance
+        ? leafExtractEntrance.libelle_matiere
+        : "extr. foliaires",
+      entrance: leafExtractEntrance ? leafExtractEntrance.total : 0,
+      exit: leafExtractExit ? leafExtractExit.total : 0,
+    },
+  ];
+  console.log(data);
+  return res.status(200).send(data);
+};
 //AJOUT OPERATION
 const addOperation = async (req, res, next) => {
   const transaction = await sequelize.transaction();
@@ -254,6 +421,7 @@ module.exports = {
   getOperationById,
   getOperationByDate,
   getOperationsByAffectation,
+  getMaterialState,
   addOperation,
   updateOperation,
   exportOperation,
